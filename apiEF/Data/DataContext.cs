a15d430dc3a3c72d0b21ed3faef64c6c46e484ae
“[Marlon <[marlonmolina10@hotmail.com]>
2022-01-25 21:50:53 +0000

    Projeto criado

---
+++ apiEF/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using apiEF.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiEF.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
